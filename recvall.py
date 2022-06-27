92e47d5562955ddb7ff4cc05f0710cf40cd78ba3
agamabergel <56891128+agamabergel@users.noreply.github.com>
2022-06-27 18:06:38 +0300

    Create recvall.py

---
+++ recvall.py
@@ -0,0 +1,19 @@
+############### Code helpers in python ###############
+
+# function 1: recvall implementation
+# This function receive all data sent back decoded
+import socket
+def recvall(sock: socket.socket) -> str:
+
+    BUFFER = 1024
+    data = ""
+
+    while True:
+        block = sock.recv(BUFFER)
+        data += block
+        if len(block) < BUFFER:
+            break
+    
+    return data
+
+
